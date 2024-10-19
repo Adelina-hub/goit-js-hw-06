ee3fd8e975766dab4e10b3d0567c0117315c1c36
Adelina-hub <adelina.macarascu@gmail.com>
2024-10-19 15:52:38 +0300

    Homework done

---
+++ js/task-07.js
@@ -0,0 +1,5 @@
+const handleInputRange = document.querySelector("#font-size-control");
+const textEl = document.querySelector("#text");
+handleInputRange.addEventListener("input", (event) => {
+  textEl.style.fontSize = event.currentTarget.value + "px";
+});
